1da7e46775761c423d1c2fc85c743566fe2ee41e
Brian McClain <brianmmcclain@gmail.com>
2022-12-06 16:25:51 -0500

    Change IP to URL

--- outputs.tf
+++ outputs.tf
@@ -1,10 +1,10 @@
 # The IP address of your test EC2 instance will be display in this output after the Terraform apply command completes.
-output "prometheus_dashboard_ip" {
+output "prometheus_dashboard_url" {
   description = "Prometheus instance dashboard"
   value       = "http://${aws_instance.prometheus_instance.public_ip}:9090/graph"
 }
 
-output "grafana_dashboard_ip" {
+output "grafana_dashboard_url" {
   description = "Grafana instance dashboard"
   value       = "http://${aws_instance.prometheus_instance.public_ip}:3000"
 }
